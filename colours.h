1da8d5a36d91adc08abe48381d7aad2533b434c4
joaovictorjs <skai7ami@gmail.com>
2023-11-30 22:07:23 -0300

    colour palette

---
+++ colours.h
@@ -0,0 +1,12 @@
+#pragma once
+
+#include <wx/colour.h>
+
+namespace Colours {
+    static wxColour primary { 249, 168, 15 };
+    static wxColour background { 245, 249, 252 };
+    static wxColour surface { 228, 233, 236 };
+    static wxColour text { 26, 40, 48 };
+    static wxColour hover { 225, 240, 252 };
+    static wxColour active { 235, 245, 252 };
+}
